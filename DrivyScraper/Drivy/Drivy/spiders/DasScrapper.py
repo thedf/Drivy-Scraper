7d44322cef8137ffd3233365cc5fdc655ec682ce
dfmanme <dfmanme@gmail.com>
2019-08-01 13:15:43 +0100

    back

--- DrivyScraper/Drivy/Drivy/spiders/DasScrapper.py
+++ DrivyScraper/Drivy/Drivy/spiders/DasScrapper.py
@@ -11,5 +11,5 @@ class MySpider(scrapy.Spider):
             yield SplashRequest(url=url, callback=self.parse,args={"wait":3})
 
     def parse(self, response):
-        content = response.xpath('//*[@id="js_picks"]/div[6]/div/div[2]/div[3]/div/div[2]/div[2]/div[1] a::attr(href)').get()
+        content = response.xpath('//*[@id="js_picks"]/div[6]/div/div[2]/div[3]/div/div[2]/div[2]/div[1]/a').get()
         yield {'article': ''.join(content)}
