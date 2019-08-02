46f9d796189b4eb0e114c75011d39f755169b616
dfmanme <dfmanme@gmail.com>
2019-08-02 01:52:02 +0100

    Update run.py

--- DrivyScraper/Drivy/Drivy/run.py
+++ DrivyScraper/Drivy/Drivy/run.py
@@ -20,7 +20,7 @@ def getArgs(argv):
 if __name__ == "__main__":
    url=getArgs(sys.argv[1:])
    crawler = CrawlerProcess()
-   crawler.crawl(MySpider,urls=[url])
+   crawler.crawl(MySpider,urls=url)
    crawler.start()
 
 
