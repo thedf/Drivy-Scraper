693066c11d917bc315898e4a9f26981899a9618a
dfmanme <dfmanme@gmail.com>
2019-08-02 01:33:15 +0100

    Update DasScrapper.py

--- DrivyScraper/Drivy/Drivy/spiders/DasScrapper.py
+++ DrivyScraper/Drivy/Drivy/spiders/DasScrapper.py
@@ -5,8 +5,7 @@ import time
 from scrapy.crawler import CrawlerProcess
 class MySpider(scrapy.Spider):
     name = "DasScrapper"
-    lien=""
-    start_urls = [lien]
+    start_urls = ""
     myclient = pymongo.MongoClient("mongodb://root:admin123@localhost:27017/")
     mydb = myclient["admin"]
     mycol = mydb["new_collection"]
