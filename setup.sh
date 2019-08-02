74ffad44cb833341a4df969ffb70b85cab6ec5bd
dfmanme <dfmanme@gmail.com>
2019-08-02 19:29:39 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
-sudo apt install docker.io
+curl -O https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
+bash Anaconda3-5.0.1-Linux-x86_64.sh
+source ~/.bashrc
+conda create --name Scrap python=3
+conda install -c conda-forge scrapy
+pip install scrapy-fake-useragent
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
 sudo docker pull scrapinghub/splash
 sudo docker run -p 8050:8050 scrapinghub/splash
-pip install scrapy-fake-useragent
