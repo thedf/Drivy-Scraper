e7b1ed7abad5be518d05b94e1217ba05e5edc7f6
dfmanme <dfmanme@gmail.com>
2019-08-04 23:48:04 +0100

    Update run.py

--- DrivyScraper/Drivy/Drivy/run.py
+++ DrivyScraper/Drivy/Drivy/run.py
@@ -24,6 +24,12 @@ CLI.add_argument(
   help='<Optional> final date',
   default= str(date. today()+datetime.timedelta(days=2)),  # default if nothing is provided
 )
+CLI.add_argument(
+  "-file",  # name on the CLI - drop the `--` for positional/required parameters
+  type=str,
+  help='<Optional> csv file ',
+  default= "result",  # default if nothing is provided
+)
 
 
 if __name__ == "__main__":
@@ -33,4 +39,4 @@ if __name__ == "__main__":
    #For each place call the Scrapper
    for i in args.places :
       url = "https://www.drivy.com/search?address="+"+".join(i.split(" "))+"&address_source=poi&poi_id=685&latitude=48.7254&longitude=2.2596&city_display_name=&start_date="+args.init+"&start_time=09%3A00&end_date="+args.end+"&end_time=09%3A00&country_scope=FR&car_sharing=true&user_interacted_with_car_sharing=false"
-      os.system("scrapy crawl DasScrapper -a start_url='"+url+"'")
+      os.system("scrapy crawl DasScrapper -a start_url='"+url+"' -o "+args.file+".csv -t csv")
