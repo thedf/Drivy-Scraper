093647db445b56b028337feccaa50a1bc7a48ae3
dfmanme <dfmanme@gmail.com>
2019-08-04 01:23:18 +0100

    adding multiples places

--- DrivyScraper/Drivy/Drivy/run.py
+++ DrivyScraper/Drivy/Drivy/run.py
@@ -1,22 +1,31 @@
-import sys, getopt
+import argparse
 import os
+from datetime import date
+import datetime
 
-def getArgs(argv):
-   try:
-      opts, args = getopt.getopt(argv,"hu:",["Url="])
-   except getopt.GetoptError:
-      print ('python run.py -u <Url>')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print ('python run.py -mg <mongodb://user:pwd@ip:port/> -db <Database> -c <Collection> -u <Url>')
-         sys.exit()
-      elif opt in ("-u", "--Url"):
-         url = arg
-   return url
+
+CLI=argparse.ArgumentParser()
+
+CLI.add_argument(
+  "-places",  # name on the CLI - drop the `--` for positional/required parameters
+  nargs="*",  # 0 or more values expected => creates a list
+  type=str,
+  default=[],  # default if nothing is provided
+)
+CLI.add_argument(
+  "-init",  # name on the CLI - drop the `--` for positional/required parameters
+  type=str,
+  default= str(date. today()+datetime.timedelta(days=1)),  # default if nothing is provided
+)
+CLI.add_argument(
+  "-end",  # name on the CLI - drop the `--` for positional/required parameters
+  type=str,
+  default= str(date. today()+datetime.timedelta(days=2)),  # default if nothing is provided
+)
+
 
 if __name__ == "__main__":
-   url=getArgs(sys.argv[1:])
-   os.system("scrapy crawl DasScrapper -a start_url='"+url+"'")
-
-
+   args = CLI.parse_args()
+   for i in args.places :
+      url = "https://www.drivy.com/search?address="+i.split(" ").join("+")+"&address_source=poi&poi_id=685&latitude=48.7254&longitude=2.2596&city_display_name=&start_date="+args.init+"&start_time=09%3A00&end_date="+args.end+"&end_time=09%3A00&country_scope=FR&car_sharing=true&user_interacted_with_car_sharing=false"
+      os.system("scrapy crawl DasScrapper -a start_url='"+url+"'")
--- setup.sh
+++ setup.sh
@@ -2,9 +2,12 @@ curl -O https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
 bash Anaconda3-5.0.1-Linux-x86_64.sh
 source ~/.bashrc
 conda create --name Scrap python=3
+source activate Scrap
 conda install -c conda-forge scrapy
 pip install scrapy-fake-useragent
-
+pip install scrapy_splash
+pip install pymongo
+pip install argparse
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
